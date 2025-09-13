63cd4f11f06118bc69ed237086e5eb22099ae93d
zdiff <zdiff@localhost>
2025-09-12 21:23:12 -0400

    Adding neovim search, indent, line number, and latency settings

--- init.lua
+++ init.lua
@@ -1,3 +1,16 @@
+vim.o.cursorcolumn = false
+vim.o.ignorecase = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.shiftwidth = 4
+vim.o.signcolumn = "yes"
+vim.o.smartcase = true
+vim.o.smartindent = true
+vim.o.tabstop = 4
+vim.o.timeoutlen = 300
+vim.o.undofile = true
+vim.o.updatetime = 250
+vim.opt.wrap = false
 vim.pack.add({
   "https://github.com/rose-pine/neovim",
 })
